ddd5b91823cb158f48f6362eb884ec030275f796
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-03-04 12:09:07 +0100

    fix: remove params from payload builder & make imports rooted

--- src/email/email_service.py
+++ src/email/email_service.py
@@ -1,15 +1,15 @@
-from auth.graph_auth import get_access_token
-from config.load_env import MAIL_USERNAME
+from src.auth.graph_auth import get_access_token
+from src.config.load_env import MAIL_USERNAME
 
-from email.email_payload_builder import build_email_payload
-from email.graph_client import send_graph_request
+from src.email.email_payload_builder import build_email_payload
+from src.email.graph_client import send_graph_request
 
 
-def send_email(subject, recipient, body):
+def send_email():
 
     access_token = get_access_token()
 
-    payload = build_email_payload(subject, recipient, body)
+    payload = build_email_payload()
 
     url = f"https://graph.microsoft.com/v1.0/users/{MAIL_USERNAME}/sendMail"
 
