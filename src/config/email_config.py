5f4689cf990af5f5afa20a59a6eedeaaede0780d
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-03-04 11:45:44 +0100

    feat: add email configuration with example data

---
+++ src/config/email_config.py
@@ -0,0 +1,7 @@
+from models.email_model import EmailModel
+
+email = EmailModel(
+    subject="Test Email from Microsoft Graph API",
+    recipient="elfouzari.soufian@gmail.com",
+    body="<p>Hello, this is a test email sent using Microsoft Graph API!</p>"
+)
