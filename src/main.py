34262aa6a67317e5ffda9c367a063bae82f78d2e
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-03-05 12:43:53 +0100

    impr: remove example data from main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,14 @@
+import email
 from src.email.email_service import send_email
 
 def main(): 
-    subject = "Test Email from Microsoft Graph API" 
-    recipient = "elfouzari.soufian@gmail.com" 
-    body = "<p>Hello, this is a test email sent using Microsoft Graph API!</p>" 
     
     try: 
         send_email() 
         print("Email sent successfully!") 
-        print("subject:", subject) 
-        print("recipient:", recipient) 
-        print("body:", body) 
+        print("subject:", email.subject) 
+        print("recipient:", email.recipient) 
+        print("body:", email.body) 
     except Exception as e: 
         print("Failed to send email:", str(e))    
               
