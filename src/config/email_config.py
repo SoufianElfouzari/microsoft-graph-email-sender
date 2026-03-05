ef21cea0b6e3434c68066920963afe7f0f830d5c
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-03-05 12:57:34 +0100

    feat: migrate recipent to List for multiple recipent function

--- src/config/email_config.py
+++ src/config/email_config.py
@@ -1,11 +1,13 @@
 from src.models.email_model import EmailModel
 from src.utils.template_loader import load_template
 
-
 html_body = load_template("base_email.html")
 
 email = EmailModel(
-    subject="Test Email from Microsoft Graph API",
-    recipient="elfouzari.soufian@gmail.com",
+    subject="Test Email from Microsoft Graph API", # TODO: Update with your Subject
+    
+    # Here you can add as many recipients as you want, simply by adding their email addresses to the list
+    recipient=["elfouzari.soufian@gmail.com", "another recipient@gmail.com"], # TODO: Update with your Recipients
+    
     body=html_body
 )
